481591dc9defc923807427b02be996cae999a4b0
Marlies Gillis <marlies.gillis@kuleuven.be>
2022-08-08 09:02:36 +0200

    files to create figure regarding reference strategy

---
+++ analysis/estimate_trfs_reference_strategy.py
@@ -0,0 +1,84 @@
+"""This script estimates TRFs for several models and saves them"""
+from pathlib import Path
+import re
+
+import eelbrain
+import mne
+import trftools
+import numpy as np
+
+
+STIMULI = [str(i) for i in range(1, 13)]
+DATA_ROOT = Path("~").expanduser() / 'Data' / 'Alice'
+PREDICTOR_DIR = DATA_ROOT / 'predictors'
+EEG_DIR = DATA_ROOT / 'eeg'
+SUBJECTS = [path.name for path in EEG_DIR.iterdir() if re.match(r'S\d*', path.name)]
+# Define a target directory for TRF estimates and make sure the directory is created
+TRF_DIR = DATA_ROOT / 'TRFs'
+TRF_DIR.mkdir(exist_ok=True)
+
+# Load stimuli
+# ------------
+# Load the broad-band envelope and process it in the same way
+envelope = [eelbrain.load.unpickle(PREDICTOR_DIR / f'{stimulus}~gammatone-1.pickle') for stimulus in STIMULI]
+envelope = [x.bin(0.01, dim='time', label='start') for x in envelope]
+envelope = [trftools.pad(x, tstart=-0.100, tstop=x.time.tstop + 1, name='envelope') for x in envelope]
+
+# Extract the duration of the stimuli, so we can later match the EEG to the stimuli
+durations = [gt.time.tmax for stimulus, gt in zip(STIMULI, envelope)]
+
+# Models
+# ------
+# Pre-define models here to have easier access during estimation. In the future, additional models could be added here and the script re-run to generate additional TRFs.
+models = {
+    # Acoustic models
+    'envelope': [envelope],
+}
+
+# Estimate TRFs
+# -------------
+# Loop through subjects to estimate TRFs
+for subject in SUBJECTS:
+    subject_trf_dir = TRF_DIR / subject
+    subject_trf_dir.mkdir(exist_ok=True)
+
+    for reference in ['cz', 'average']:
+        # Generate all TRF paths so we can check whether any new TRFs need to be estimated
+        trf_paths = {model: subject_trf_dir / f'{subject} {model}_{reference}.pickle' for model in models}
+        # Skip this subject if all files already exist
+        if all(path.exists() for path in trf_paths.values()):
+            continue
+        # Load the EEG data
+        raw = mne.io.read_raw(EEG_DIR / subject / f'{subject}_alice-raw_{reference}.fif', preload=True)
+        # Band-pass filter the raw data between 0.2 and 20 Hz
+        raw.filter(0.5, 20)
+        # Interpolate bad channels
+        raw.interpolate_bads()
+        # Extract the events marking the stimulus presentation from the EEG file
+        events = eelbrain.load.fiff.events(raw)
+        # Not all subjects have all trials; determine which stimuli are present
+        trial_indexes = [STIMULI.index(stimulus) for stimulus in events['event']]
+        # Extract the EEG data segments corresponding to the stimuli
+        trial_durations = [durations[i] for i in trial_indexes]
+        eeg = eelbrain.load.fiff.variable_length_epochs(events, -0.100, trial_durations, decim=5, connectivity='auto')
+        # Since trials are of unequal length, we will concatenate them for the TRF estimation.
+        eeg_concatenated = eelbrain.concatenate(eeg)
+
+        if reference in ['cz']: 
+            Cz_location = [label_idx for label_idx, label in enumerate(eeg_concatenated.sensor.names) if label == '33']
+            eeg_concatenated.x[Cz_location,] = np.random.randn(eeg_concatenated[Cz_location,].x.shape[0], eeg_concatenated[Cz_location,].x.shape[1])*np.mean(eeg_concatenated.x)
+
+        for model, predictors in models.items():
+            path = trf_paths[model]
+            # Skip if this file already exists
+            if path.exists():
+                continue
+            print(f"Estimating: {subject} ~ {model} ~ {reference}")
+            # Select and concetenate the predictors corresponding to the EEG trials
+            predictors_concatenated = []
+            for predictor in predictors:
+                predictors_concatenated.append(eelbrain.concatenate([predictor[i] for i in trial_indexes]))
+            # Fit the mTRF
+            trf = eelbrain.boosting(eeg_concatenated, predictors_concatenated, -0.100, 1.000, error='l1', basis=0.050, partitions=5, test=1, selective_stopping=True)
+            # Save the TRF for later analysis
+            eelbrain.save.pickle(trf, path)
---
+++ figures/Reference-strategy.py
@@ -0,0 +1,117 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.11.4
+#   kernelspec:
+#     display_name: tutorialAlice
+#     language: python
+#     name: tutorialalice
+# ---
+
+# +
+from pathlib import Path
+
+import eelbrain
+from matplotlib import pyplot
+import mne
+import re
+import trftools
+import os
+# -
+
+# Data locations
+DATA_ROOT = Path("~").expanduser() / 'Data' / 'Alice'
+PREDICTOR_DIR = DATA_ROOT / 'predictors'
+STIMULI_DIR = DATA_ROOT / 'stimuli'
+TRF_DIR = DATA_ROOT / 'TRFs'
+EPOCH_DIR = DATA_ROOT / 'Epochs'
+
+# Where to save the figure
+DST = DATA_ROOT / 'figures'
+DST.mkdir(exist_ok=True)
+
+# Configure the matplotlib figure style
+FONT = 'Helvetica Neue'
+FONT_SIZE = 8
+RC = {
+    'figure.dpi': 150,
+    'savefig.dpi': 300,
+    'savefig.transparent': True,
+    # Font
+    'font.family': 'sans-serif',
+    'font.sans-serif': FONT,
+    'font.size': FONT_SIZE,
+    'axes.labelsize': FONT_SIZE,
+    'axes.titlesize': FONT_SIZE,
+}
+pyplot.rcParams.update(RC)
+# -
+
+# get all subjects
+subjects = [subject for subject in os.listdir(TRF_DIR) if subject.startswith('S') ]
+
+# +
+cases = []
+for subject in subjects:
+    for reference, name in zip(['mastoids','cz','average'], ['envelope.pickle','envelope_cz.pickle','envelope_average.pickle']):
+        mtrf = eelbrain.load.unpickle(f"{TRF_DIR}/{subject}/{subject} {name}")
+        trf = mtrf.h[0]
+        prediction_accuracy = mtrf.r
+        cases.append([subject, trf, prediction_accuracy, reference])
+
+column_names = ['subject', 'trf', 'prediction_accuracy','reference']
+data_trfs = eelbrain.Dataset.from_caselist(column_names, cases, random='subject')
+# -
+
+# Initialize figure
+figure = pyplot.figure(figsize=(7.5, 5))
+gridspec = figure.add_gridspec(9,9, height_ratios=[2,2,2,2,2,2,2,2,2], left=0.05, right=0.95, hspace=1.5)
+topo_args = dict(clip='circle')
+det_args = dict(**topo_args, vmax=0.06, cmap='lux-a')
+cbar_args = dict(label='Pearson\'s r', ticks=2, h=2)
+
+# 1) plot the prediction accuracies 
+for reference_idx, reference in enumerate(['mastoids','cz','average']): 
+    axes = figure.add_subplot(gridspec[reference_idx*3:reference_idx*3+3,0:3])
+    p = eelbrain.plot.Topomap('prediction_accuracy', ds=data_trfs[data_trfs['reference']==reference], axes=axes, **det_args)
+    axes.set_title("Reference to %s" % reference, loc='left')
+p.plot_colorbar(below=axes, **cbar_args)
+
+
+# plot the TRFs 
+times = [0.04, 0.14, 0.24]
+for reference_idx, reference in enumerate(['mastoids','cz','average']): 
+    axes = figure.add_subplot(gridspec[reference_idx*3:reference_idx*3+3,3:9])
+    
+    # plot butterfly TRF
+    res = eelbrain.testnd.TTestOneSample('trf', ds=data_trfs[data_trfs['reference']==reference], pmin=0.05)
+    
+    if reference_idx != 2:
+        plot = eelbrain.plot.Butterfly(res, axes=axes, vmin=-0.004, vmax=0.007, color = '#808080', ylabel='TRF weights [a.u.]', frame='t', yticklabels='none', xticklabels='none', xlabel='')
+    else:
+         plot = eelbrain.plot.Butterfly(res, axes=axes, vmin=-0.004, vmax=0.007, color = '#808080', ylabel='TRF weights [a.u.]', frame='t', yticklabels='none')   
+    for time in times: 
+        plot.add_vline(time, color='r',linestyle='--')
+
+    # plot topomaps
+    axes = [figure.add_subplot(gridspec[reference_idx*3:reference_idx*3+2,6]), 
+           figure.add_subplot(gridspec[reference_idx*3:reference_idx*3+2,7]), 
+           figure.add_subplot(gridspec[reference_idx*3:reference_idx*3+2,8])]
+    time_labels=['%d ms' % (time*1000) for time in times]
+    plot_topo = eelbrain.plot.Topomap([data_trfs[data_trfs['reference']==reference, 'trf'].sub(time=time) for time in times], axes=axes, axtitle=time_labels, ncol=len(times), head_radius=0.45, clip='circle')
+    plot_topo.plot_colorbar(right_of=axes[-1], label='', label_rotation=90, ticks={0.006:'+', -0.006:'-', 0.000:'0'})
+    
+
+figure.text(0.08, 0.96, 'A) Prediction accuracy', size=10)
+figure.text(0.35, 0.96, 'B) Envelope TRF', size=10)
+
+figure
+
+figure.savefig(DST / 'Reference-strategy.pdf')
+figure.savefig(DST / 'Reference-strategy.png')
+
+
