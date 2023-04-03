e87dd5a96bd250320505a5f60e4b3f2fb04b9721
Christian Brodbeck <christianmbrodbeck@gmail.com>
2023-04-03 16:41:32 -0400

    DOC/update predictors
    Update high-res gammatone method

--- predictors/make_gammatone_predictors.py
+++ predictors/make_gammatone_predictors.py
@@ -1,29 +1,36 @@
-"""Predictors based on gammatone spectrograms"""
+"""
+Predictors based on gammatone spectrograms
+
+Assumes that ``make_gammatone.py`` has been run to create the high resolution
+spectrograms.
+"""
 from pathlib import Path
 
-import numpy as np
 import eelbrain
 
+
+# Define paths to data, and destination for predictors
 DATA_ROOT = Path("~").expanduser() / 'Data' / 'Alice'
 STIMULUS_DIR = DATA_ROOT / 'stimuli'
 PREDICTOR_DIR = DATA_ROOT / 'predictors'
 
+# If the directory for predictors does not exist yet, create it
 PREDICTOR_DIR.mkdir(exist_ok=True)
+# Loop through stimuli
 for i in range(1, 13):
+    # Load the high resolution gammatone spectrogram
     gt = eelbrain.load.unpickle(STIMULUS_DIR / f'{i}-gammatone.pickle')
 
-    # Remove resampling artifacts
-    gt = gt.clip(0, out=gt)
-    # apply log transform
+    # Apply a log transform to approximate peripheral auditory processing
     gt = (gt + 1).log()
-    # generate onset detector model
+    # Apply the edge detector model to generate an acoustic onset spectrogram
     gt_on = eelbrain.edge_detector(gt, c=30)
 
-    # 1 band predictors
+    # Create and save 1 band versions of the two predictors (i.e., temporal envelope predictors)
     eelbrain.save.pickle(gt.sum('frequency'), PREDICTOR_DIR / f'{i}~gammatone-1.pickle')
     eelbrain.save.pickle(gt_on.sum('frequency'), PREDICTOR_DIR / f'{i}~gammatone-on-1.pickle')
-    # 8 band predictors
-    x = gt.bin(nbins=8, func=np.sum, dim='frequency')
+    # Create and save 8 band versions of the two predictors (binning the frequency axis into 8 bands)
+    x = gt.bin(nbins=8, func='sum', dim='frequency')
     eelbrain.save.pickle(x, PREDICTOR_DIR / f'{i}~gammatone-8.pickle')
-    x = gt_on.bin(nbins=8, func=np.sum, dim='frequency')
+    x = gt_on.bin(nbins=8, func='sum', dim='frequency')
     eelbrain.save.pickle(x, PREDICTOR_DIR / f'{i}~gammatone-on-8.pickle')
